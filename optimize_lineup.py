891ad52c23376c272b636f7c8425bc0611b5f072
Madison-PD <85171390+Madison-PD@users.noreply.github.com>
2023-10-31 22:19:22 -0500

    Update optimize_lineup.py

--- optimize_lineup.py
+++ optimize_lineup.py
@@ -3,9 +3,10 @@ import numpy as np
 
 
 class Optimized_Lineups:
-    def __init__(self, owner, data):
+    def __init__(self, owner, data, optimize_col='z'):
         self.owner = owner
-        self.data = data
+        self.optimize_col = optimize_col
+        self.data = data.sort_values(optimize_col, ascending=False)
         self.num_hitter_positions = 14
         self.d = self.data[self.data['Owner']==owner][['Player','all_pos', 'z', 'type']].set_index('Player').to_dict(orient='index')      
         self.p_dict = {k:v for (k,v) in self.d.items() if 'p' in v['type']}
@@ -42,27 +43,38 @@ class Optimized_Lineups:
         return z_list
     
     def _make_hitter_combos(self):
-        inf = [i for i in product(self.catchers, self.first_basemen, self.second_basemen, self.shortstops, self.third_basemen, self.middles, self.corners, self.ofs)]
-        _list = []
-        for num in range(len(inf)):
-            _list.append([item for item in inf[num] if type(item)!=tuple]+[item for sublist in inf[num] for item in sublist if type(sublist)==tuple])
+        position_combos = [i for i in product(self.catchers, self.first_basemen, self.second_basemen, self.shortstops, self.third_basemen, self.middles, self.corners, self.ofs)]
+        max_z = -999
+        max_z_idx = -1
+        for num in range(len(position_combos)):
+            list_z = sum([self.h_dict[name]['z'] for name in position_combos[num] if type(name)!=tuple]+[self.h_dict[item]['z'] for sublist in position_combos[num] for item in sublist if type(sublist)==tuple])
+            if list_z > max_z and len(set(list(position_combos[num][0:-1])+list(position_combos[num][-1]))) == 12:
+                    max_z = list_z
+                    max_z_idx = num
         
-        del(inf)
-        _list2 = [i for i in _list if len(set(i))==12]
-        del(_list)
-        inf2 = [i for i in product(_list2, self.dh2)]
+        base_12 = list(set(list(position_combos[max_z_idx][0:-1])+list(position_combos[max_z_idx][-1])))
+        self.hitter_optimized_lineup = list(position_combos[max_z_idx][0:-1])+list(position_combos[max_z_idx][-1])+[i for i in self.h_dict.keys() if i not in base_12][:2]
+        self.hitter_optimized_z = self.data[self.data['Player'].isin(self.hitter_optimized_lineup)]['z'].sum()
+        #_list = []
+        #for num in range(len(inf)):
+        #    _list.append([item for item in inf[num] if type(item)!=tuple]+[item for sublist in inf[num] for item in sublist if type(sublist)==tuple])
         
-        del(_list2)
-        _list3 = []
-        for num in range(len(inf2)):
-            _list3.append([list(i) for i in inf2[num] if type(i)!=tuple][0]+[list(i) for i in inf2[num] if type(i)==tuple][0])
-        del(inf2)
+        #del(inf)
+        #_list2 = [i for i in _list if len(set(i))==12]
+        #del(_list)
+        #inf2 = [i for i in product(_list2, self.dh2)]
         
-        self.hitter_combos = [i for i in _list3 if len(set(i))==self.num_hitter_positions]
-        print(len(self.hitter_combos))
-        self.hitter_z_list = self._z_list(self.hitter_combos, self.h_dict)
+        #del(_list2)
+        #_list3 = []
+        #for num in range(len(inf2)):
+            #_list3.append([list(i) for i in inf2[num] if type(i)!=tuple][0]+[list(i) for i in inf2[num] if type(i)==tuple][0])
+        #del(inf2)
+
+        #self.hitter_combos = [i for i in _list3 if len(set(i))==self.num_hitter_positions]
+        #print(len(self.hitter_combos))
+        #self.hitter_z_list = self._z_list(self.hitter_combos, self.h_dict)
         #print(len(self.hitter_z_list))
-        self.hitter_idx = np.nanargmax(self.hitter_z_list)
-        self.hitter_optimized_z = self.hitter_z_list[self.hitter_idx]
-        self.hitter_optimized_lineup = self.hitter_combos[self.hitter_idx]
+        #self.hitter_idx = np.nanargmax(self.hitter_z_list)
+        #self.hitter_optimized_z = self.hitter_z_list[self.hitter_idx]
+        #self.hitter_optimized_lineup = self.hitter_combos[self.hitter_idx]
         return    
